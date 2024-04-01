78eafbe5eaf02db5fa0bc7aa311653732fc61bf8
Matheus Felipe da Silva <matheusfelipe.silva160205@gmail.com>
2024-03-31 22:25:22 -0300

    Create Celula.cs

---
+++ Celula/Celula.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista-Celula
+{
+    internal class Celula
+    {
+        private String item;
+        private Celula proximo;
+        public String Item
+        {
+            get { return item; }
+            set { item = value; }
+        }
+        public Celula Proximo
+        {
+            get { return proximo;}
+            set { proximo = value;}
+        }
+        public Celula(String novo)
+        {
+            item = novo;
+            proximo = null;
+        }
+        public Celula()
+        {
+            item = new String();
+            proximo = null;
+        }
+    }
+}
