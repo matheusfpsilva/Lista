9cc4223fb46892b063f7cdf6789b344a45897c25
Matheus Felipe da Silva <matheusfelipe.silva160205@gmail.com>
2024-03-31 22:23:49 -0300

    Create Site.cs

---
+++ Vetor/Site.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista-Vetor
+{
+    internal class Site
+    {
+        private string nome;
+        private string link;
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+        public string Link
+        {
+            get { return link; }
+            set { link = value; }
+        }
+        public Site(string nome, string link)
+        {
+            this.nome = nome;
+            this.link = link;
+        }
+        public Site()
+        {
+            nome = null;
+            link = null;
+        }
+        public void imprimir()
+        {
+            Console.WriteLine("Nome: " + nome + ", " + "Link: " + link);
+        }
+    }
+}
