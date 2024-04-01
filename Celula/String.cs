ea5069c05de4a8eb758f36ba2e719909c2c6ae5b
Matheus Felipe da Silva <matheusfelipe.silva160205@gmail.com>
2024-03-31 22:25:58 -0300

    Create String.cs

---
+++ Celula/String.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista-Celula
+{
+    internal class String
+    {
+        private string valor;
+        public string Valor
+        {
+            get { return valor; } 
+            set { valor = value;}
+        }
+        public String(string valor)
+        {
+            this.valor = valor;
+        }
+        public String()
+        {
+            valor = null;
+        }
+        public void imprimir()
+        {
+            Console.WriteLine(Valor);
+        }
+    }
+}
